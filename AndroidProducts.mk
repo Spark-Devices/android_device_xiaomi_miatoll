FireFly009/decommonized-device_xiaomi_miatoll

miatoll: Decommonize
- Decommonized from: https://github.com/LineageOS/android_device_xiaomi_miatoll/tree/lineage-19.1
- All credits to original contributors

Signed-off-by: zlewchan <x@zlew.tf>

AndroidProducts.mk
@@ -1,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/xdroid_miatoll.mk
+
+COMMON_LUNCH_CHOICES := \
+    xdroid_miatoll-user \
+    xdroid_miatoll-userdebug \
+    xdroid_miatoll-eng

Spark-Devices/android_device_xiaomi_miatoll

AndroidProducts.mk
@@ -1,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/tequila_miatoll.mk
+
+COMMON_LUNCH_CHOICES := \
+    tequila_miatoll-user \
+    tequila_miatoll-userdebug \
+    tequila_miatoll-eng