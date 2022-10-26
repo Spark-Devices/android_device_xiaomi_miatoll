2388996eea9f35648911071352148874c1963e03
popoA3M <pratyayaborborah@gmail.com>
2022-10-26 14:40:00 +0530

    miatoll: Adapt trees for SparkOS
    Signed-off-by: popoA3M <pratyayaborborah@gmail.com>

--- spark_miatoll.mk
+++ spark_miatoll.mk
@@ -8,17 +8,21 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from the tequilaOS configuration.
-$(call inherit-product, vendor/tequila/config/common_full_phone.mk)
+# Inherit from the SparkOS configuration.
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
 # Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-PRODUCT_NAME := tequila_miatoll
+PRODUCT_NAME := spark_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := SM6250
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
