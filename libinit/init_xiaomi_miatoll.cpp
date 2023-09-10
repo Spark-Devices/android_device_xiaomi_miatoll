55829e343833e9fb5da906737f917166f63bcad1
Giovanni Ricca <giovanniricca@proton.me>
2023-09-10 19:17:27 +0530

    miatoll: Move NFC handling out of libinit
    * It doesn't work on GSI and DSU
    
    Signed-off-by: popoA3M <pratyayaborborah@gmail.com>

--- libinit/init_xiaomi_miatoll.cpp
+++ libinit/init_xiaomi_miatoll.cpp
@@ -18,7 +18,6 @@ static const variant_info_t curtanain_info = {
     .model = "Redmi Note 10 Lite",
     .build_fingerprint = "Redmi/curtana_in1/curtana:12/RKQ1.211019.001/V14.0.5.0.SJWINRF:user/release-keys",
 
-    .nfc = false,
 };
 
 static const variant_info_t curtana_info = {
@@ -30,7 +29,6 @@ static const variant_info_t curtana_info = {
     .model = "Redmi Note 9S",
     .build_fingerprint = "Redmi/curtana_global/curtana:12/RKQ1.211019.001/V14.0.4.0.SJWMIXM:user/release-keys",
 
-    .nfc = false,
 };
 
 static const variant_info_t excalibur_info = {
@@ -42,7 +40,6 @@ static const variant_info_t excalibur_info = {
     .model = "Redmi Note 9 Pro Max",
     .build_fingerprint = "Redmi/excalibur_in/excalibur:12/RKQ1.211019.001/V14.0.2.0.SJXINXM:user/release-keys",
 
-    .nfc = false,
 };
 
 static const variant_info_t gram_info = {
@@ -54,7 +51,6 @@ static const variant_info_t gram_info = {
     .model = "POCO M2 Pro",
     .build_fingerprint = "POCO/gram_in/gram:12/RKQ1.211019.001/V14.0.5.0.SJPINXM:user/release-keys",
 
-    .nfc = false,
 };
 
 static const variant_info_t joyeuse_info = {
@@ -66,7 +62,6 @@ static const variant_info_t joyeuse_info = {
     .model = "Redmi Note 9 Pro",
     .build_fingerprint = "Redmi/joyeuse_global/joyeuse:12/RKQ1.211019.001/V14.0.3.0.SJZMIXM:user/release-keys",
 
-    .nfc = true,
 };
 
 static const std::vector<variant_info_t> variants = {
